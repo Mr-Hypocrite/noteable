6561fdd2a5ab07fa3ec1a3779ac3162fcabb9898
Jatin <ym.jatin@gmail.com>
2025-03-02 11:34:45 +0530

    chore: set up and configure panda css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Flex } from 'styled-system/jsx';
+
 function App() {
-    return <></>;
+    return (
+        <Flex
+            direction={'column'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            w={'full'}
+            minH={'screen'}
+        >
+            <h1>noteable</h1>
+            <p>capture, organize, and remember – your thoughts, your way.</p>
+        </Flex>
+    );
 }
 
 export default App;
