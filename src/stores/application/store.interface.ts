dee2b578979fbbc48a54c1610ad53cfdd1b0e532
Jatin <ym.jatin@gmail.com>
2025-03-02 16:07:16 +0530

    chore: add store interface

---
+++ src/stores/application/store.interface.ts
@@ -0,0 +1,19 @@
+import { INote, INoteModifiableContents } from '@/utils/interfaces';
+import { Draft } from 'immer';
+
+export interface INoteActions {
+    createNote: (note: Partial<INoteModifiableContents>) => void;
+    editNote: (id: string, note: Partial<INoteModifiableContents>) => void;
+    deleteNote: (id: string) => void;
+    deleteNotes: (ids: string[]) => void;
+    pinNote: (id: string) => void;
+    pinNotes: (ids: string[]) => void;
+}
+
+export interface INoteState {
+    notes: Record<string, INote>;
+}
+
+export interface INoteStore extends INoteState {}
+
+export type INoteStoreSetter = (updater: (draft: Draft<INoteStore>) => void) => void;
