9c48cd79f460b8edb44a348ac121538d809b05f1
Jatin <ym.jatin@gmail.com>
2025-03-05 23:54:26 +0530

    chore: add spinner component

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,5 +2,6 @@ export * from './button';
 export * from './editable';
 export * from './icon-button';
 export * from './note-action-buttons';
+export * from './spinner';
 export * from './text';
 export * from './tooltip';
