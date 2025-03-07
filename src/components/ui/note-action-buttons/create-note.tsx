29ddd5d72256634f3c2559333d4668ad817f5197
Jatin <ym.jatin@gmail.com>
2025-03-07 17:09:01 +0530

    feat: impl create note logic

--- src/components/ui/note-action-buttons/create-note.tsx
+++ src/components/ui/note-action-buttons/create-note.tsx
@@ -3,14 +3,22 @@ import { PlusIcon } from 'lucide-react';
 import { FC } from 'react';
 import { IconButton, IconButtonProps } from '../icon-button';
 import { Tooltip } from '../tooltip';
+import { useStoreActions } from '@/stores';
 
 export interface CreateNoteProps extends IconButtonProps, Partial<INoteModifiableContents> {}
 
+const newNote: Partial<INoteModifiableContents> = {
+    noteTitle: '',
+    noteContent: '',
+    pinned: false
+};
+
 export const CreateNote: FC<CreateNoteProps> = ({ ...props }) => {
+    const { createNote } = useStoreActions();
     return (
         <Tooltip.Root>
             <Tooltip.Trigger asChild>
-                <IconButton {...props}>
+                <IconButton colorPalette={'emerald'} onClick={() => createNote(newNote)} {...props}>
                     <PlusIcon />
                 </IconButton>
             </Tooltip.Trigger>
