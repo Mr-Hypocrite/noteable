89d55298481742e565d7f36479bcb88faacd9e9b
Jatin <ym.jatin@gmail.com>
2025-03-07 17:12:48 +0530

    chore: export note from components

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,6 +2,7 @@ export * from './button';
 export * from './editable';
 export * from './header';
 export * from './icon-button';
+export * from './note';
 export * from './note-action-buttons';
 export * from './spinner';
 export * from './text';
