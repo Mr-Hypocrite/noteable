415fd154d62c11c64aefeff3a5afc42eac7da67b
Jatin <ym.jatin@gmail.com>
2025-03-07 17:18:13 +0530

    wip: show notes if any on the home screen

--- src/components/misc/home/home.tsx
+++ src/components/misc/home/home.tsx
@@ -1,13 +1,41 @@
+import { CreateNote, Header, Text } from '@/components';
+import { NotesGrid } from '@/components/ui';
+import { useGetNotes } from '@/stores';
 import { FC } from 'react';
+import { Flex } from 'styled-system/jsx';
 import { InitContent } from './init-content';
-import { CreateNote } from '@/components';
 
 export interface HomeProps {}
 
 export const Home: FC<HomeProps> = () => {
+    const notes = useGetNotes();
+    const pinnedNotes = useGetNotes({ pinned: true });
+
     return (
         <>
-            <InitContent />
+            {notes.length > 0 || pinnedNotes.length > 0 ? (
+                <>
+                    <Header />
+                    {pinnedNotes.length > 0 ? (
+                        <Flex w={'full'} direction={'column'} gap={'4'} mb={'8'}>
+                            <Text as={'h3'} fontWeight={'light'} size={'md'}>
+                                pinned notes:
+                            </Text>
+                            <NotesGrid notes={pinnedNotes} />
+                        </Flex>
+                    ) : null}
+                    {notes.length > 0 ? (
+                        <Flex w={'full'} direction={'column'} gap={'4'} mb={'8'}>
+                            <Text as={'h3'} fontWeight={'light'} size={'md'}>
+                                notes:
+                            </Text>
+                            <NotesGrid notes={notes} />
+                        </Flex>
+                    ) : null}
+                </>
+            ) : (
+                <InitContent />
+            )}
             <CreateNote pos={'fixed'} right={'6'} bottom={'6'} />
         </>
     );
