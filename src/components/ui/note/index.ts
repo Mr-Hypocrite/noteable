e2cd0a9d6ae81c9456045b7349d3f658b9e55798
Jatin <ym.jatin@gmail.com>
2025-03-11 15:30:56 +0530

    chore: note exports

--- src/components/ui/note/index.ts
+++ src/components/ui/note/index.ts
@@ -1,2 +1,5 @@
+export * from './editor-toolbar.tsx';
+export * from './note-editable-content.tsx';
+export * from './note-editable-title.tsx';
 export * from './note.tsx';
 export * from './notes-grid.tsx';
