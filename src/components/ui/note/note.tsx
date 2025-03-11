e09fdd795f413ba10b3b3efa00f028582f9467e4
Jatin <ym.jatin@gmail.com>
2025-03-11 15:30:15 +0530

    perf: impl debounced edit
    reduce the amount of rewrites on the store

--- src/components/ui/note/note.tsx
+++ src/components/ui/note/note.tsx
@@ -1,23 +1,47 @@
 import { useStoreActions } from '@/stores';
 import { INote } from '@/utils';
-import { FC } from 'react';
+import debounce from 'debounce';
+import { FC, useCallback, useEffect, useState } from 'react';
 import { Flex, FlexProps } from 'styled-system/jsx';
 import { IconButtonProps } from '../icon-button';
 import { DeleteNote, PinNote } from '../note-action-buttons';
+import { EditableContent } from './note-editable-content';
 import { NoteEditableTitle } from './note-editable-title';
 
 export interface NoteProps extends INote {}
 
-export const Note: FC<NoteProps> = ({ id, created_at, pinned, noteTitle, noteContent }) => {
+export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent }) => {
     const { togglePinNote, deleteNote, editNote } = useStoreActions();
+    const [localNoteTitle, setLocalNoteTitle] = useState(noteTitle);
+    const [localNoteContent, setLocalNoteContent] = useState(noteContent);
+    const debouncedEditNote = useCallback(
+        debounce((id: string, _noteTitle: string) => editNote(id, { noteTitle: _noteTitle }), 500),
+        [id]
+    );
+    const debouncedEditContent = useCallback(
+        debounce((id: string, _noteContent) => editNote(id, { noteContent: _noteContent }), 500),
+        [id]
+    );
+
+    useEffect(() => {
+        debouncedEditNote(id, localNoteTitle);
+    }, [localNoteTitle]);
+
+    useEffect(() => {
+        debouncedEditContent(id, localNoteContent);
+    }, [localNoteContent]);
 
     return (
         <Flex id={id} className="group" {...noteStyles}>
             <NoteEditableTitle
                 inputProps={{
-                    onChange: (event) => editNote(id, { noteTitle: event.target.value })
+                    onChange: (event) => setLocalNoteTitle(event.target.value)
                 }}
-                value={noteTitle}
+                value={localNoteTitle}
+            />
+            <EditableContent
+                noteContent={localNoteContent}
+                onChange={(value) => setLocalNoteContent(value)}
             />
             <PinNote
                 {...noteActionBtnCommonStyles}
