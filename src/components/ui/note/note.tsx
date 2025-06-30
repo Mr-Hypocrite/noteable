2c6729bc75f85e625521651b7a1380d86ffee7f3
Jatin <ym.jatin@gmail.com>
2025-06-30 16:56:32 +0530

    chore: note-actions impl

--- src/components/ui/note/note.tsx
+++ src/components/ui/note/note.tsx
@@ -1,23 +1,21 @@
-import { useStoreActions } from '@/stores';
+import { useNoteStoreActions } from '@/stores';
 import { INote } from '@/utils';
 import debounce from 'debounce';
 import { FC, useCallback, useEffect, useState } from 'react';
 import { Flex, FlexProps } from 'styled-system/jsx';
-import { IconButtonProps } from '../icon-button';
-import { DeleteNote, PinNote } from '../note-action-buttons';
+import { NoteActions } from './note-actions';
 import { EditableContent } from './note-editable-content';
 import { NoteEditableTitle } from './note-editable-title';
-import { DeleteNoteConfirmation } from '../confirmation-dialog';
 
-export interface NoteProps extends INote {}
 export interface NoteProps extends INote {
     selected: boolean;
 }
 
-export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent }) => {
-    const { togglePinNote, editNote } = useStoreActions();
+export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent, selected }) => {
+    const { editNote } = useNoteStoreActions();
     const [localNoteTitle, setLocalNoteTitle] = useState(noteTitle);
     const [localNoteContent, setLocalNoteContent] = useState(noteContent);
+    const [hoverState, setHoverState] = useState(false);
     const debouncedEditNote = useCallback(
         debounce((id: string, _noteTitle: string) => editNote(id, { noteTitle: _noteTitle }), 500),
         [id]
@@ -36,7 +34,13 @@ export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent }) => {
     }, [localNoteContent]);
 
     return (
-        <Flex id={id} className="group" {...noteStyles}>
+        <Flex
+            id={id}
+            className="group"
+            onMouseOver={() => setHoverState(true)}
+            onMouseOut={() => setHoverState(false)}
+            {...noteStyles}
+        >
             <NoteEditableTitle
                 inputProps={{
                     onChange: (event) => setLocalNoteTitle(event.target.value)
@@ -47,16 +51,7 @@ export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent }) => {
                 noteContent={localNoteContent}
                 onChange={(value) => setLocalNoteContent(value)}
             />
-            <PinNote
-                {...noteActionBtnCommonStyles}
-                onClick={() => togglePinNote(id)}
-                pinned={pinned}
-                noteId={id}
-                top={{ base: '0', md: '-2' }}
-            />
-            <DeleteNoteConfirmation noteId={id}>
-                <DeleteNote {...noteActionBtnCommonStyles} bottom={{ base: '0', md: '-2' }} />
-            </DeleteNoteConfirmation>
+            <NoteActions id={id} pinned={pinned} selected={selected} hoverState={hoverState} />
         </Flex>
     );
 };
@@ -64,25 +59,14 @@ export const Note: FC<NoteProps> = ({ id, pinned, noteTitle, noteContent }) => {
 const noteStyles: FlexProps = {
     bgColor: { base: 'emerald.3', _hover: 'emerald.4' },
     color: { base: 'fg.default' },
-    gap: '1',
+    gap: '2',
     transition: 'background',
     transitionDuration: 'normal',
     transitionTimingFunction: 'ease-in-out',
     borderRadius: 'md',
     direction: 'column',
-    minH: 'sm',
-    maxH: 'sm',
+    aspectRatio: '2/3',
+    h: 'auto',
     p: '4',
     position: 'relative'
-};
-
-const noteActionBtnCommonStyles: IconButtonProps = {
-    visibility: 'hidden',
-    size: { base: 'sm', md: 'md' },
-    _groupHover: {
-        visibility: 'visible'
-    },
-    colorPalette: 'emerald',
-    position: 'absolute',
-    right: { base: '0', md: '-2' }
 };
